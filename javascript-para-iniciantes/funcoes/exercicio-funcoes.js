a2b64bf54baa9faa76e532a724421a2223102e60
guuhgst <gustavoprogramador18@gmail.com>
2022-08-31 02:02:50 -0300

    docs: início de exercício

---
+++ javascript-para-iniciantes/funcoes/exercicio-funcoes.js
@@ -0,0 +1,30 @@
+// Crie uma função para verificar se um valor é Truthy
+
+// Crie uma função matemática que retorne o perimetro de um quadrado
+// lembrando: perimetro é a soma dos quatro lados do quadrado
+
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+
+// Crie uma função que verifica se um número é par
+
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento click ocorrer.
+
+// Corrija o erro abaixo
+function precisoVisitar(paisesVisitados) {
+    var totalPaises = 193;
+    return `Ainda faltam ${totalPaises - paisesVisitados} paises para visitar`;
+}
+
+function jaVisitei(paisesVisitados) {
+    return `Já visitei ${paisesVisitados} do total de ${totalPaises} paises`;
+}
+
+precisoVisitar(20);
+jaVisitei(20);
