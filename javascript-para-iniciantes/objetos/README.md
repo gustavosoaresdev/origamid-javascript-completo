6c1c77e08bd20146ac9bd44b7a0f9e6aca99bc74
guuhgst <gustavoprogramador18@gmail.com>
2022-09-01 01:18:03 -0300

    docs: métodos

--- javascript-para-iniciantes/objetos/README.md
+++ javascript-para-iniciantes/objetos/README.md
@@ -19,3 +19,23 @@ propriedades e métodos.
 
 *Propriedades e métodos consistem*
 *em nome (chave) e valor*
+
+## Métodos
+
+É uma propriedade que possui uma função no local do seu valor.
+
+```js
+    var quadrado = {
+        lados: 4,
+        area: function(lado) {
+            return lado * lado;
+        },
+        perimetro: function(lado) {
+            return this.lados * lado;
+        },
+    }
+
+    quadrado.lados; // 4
+    quadrado.area(5); // 25
+    quadrado.perimetro(5); // 20
+```
--- javascript-para-iniciantes/objetos/script.js
+++ javascript-para-iniciantes/objetos/script.js
@@ -6,3 +6,18 @@ var pessoa = {
 console.log(pessoa.nome);   //  'Gustavo'
 
 /* ================================================================================= */
+
+var quadrado = {
+    lados: 4,
+    area: function (lado) {
+        return lado * lado;
+    },
+    perimetro: function (lado) {
+        return this.lados * lado;
+    }
+};
+
+console.log(quadrado.area(5));  //  25
+console.log(quadrado.perimetro(5)); //  20
+
+/* ================================================================================= */
