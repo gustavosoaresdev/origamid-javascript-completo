c2fe72b47cc703e954385b9a1e27f17725cae4bb
guuhgst <gustavoprogramador18@gmail.com>
2022-09-03 05:06:57 -0300

    docs: :white_check_mark: exercício concluido!

--- javascript-para-iniciantes/arrays-e-loops/exercicio-arrays-e-loops.js
+++ javascript-para-iniciantes/arrays-e-loops/exercicio-arrays-e-loops.js
@@ -1,11 +1,22 @@
 // Crie uma array com os anos que o Brasil ganhou a copa
 // 1959, 1962, 1970, 1994, 2002
+var brasilCampeao = [1959, 1962, 1970, 1994, 2002];
 
 // Interaja com a array utilizando um loop, para mostrar
 // no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+brasilCampeao.forEach(function (ano) {
+    console.log(`Brasil ganhou a copa de ${ano}`);
+});
 
 // Interaja com um loop nas frutas abaixo e pare ao chegar em Pêra
-var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância'];
+var frutas = ['Banana', 'Maçã', 'Pêra', 'Uva', 'Melância'];
+for (var i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+    if (frutas[i] === 'Pêra') {
+        break;
+    }
+}
 
 // Coloque a última fruta da array acima em uma variável,
 // sem remover a mesma da array.
+var ultimaFruta = frutas[frutas.length - 1];
