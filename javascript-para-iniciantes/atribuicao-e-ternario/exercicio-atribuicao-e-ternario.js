09a766fefe2b2bc223a32248555b2d86e194c0cb
guuhgst <gustavoprogramador18@gmail.com>
2022-09-04 02:51:05 -0300

    docs: :white_check_mark: exercício concluido!

--- javascript-para-iniciantes/atribuicao-e-ternario/exercicio-atribuicao-e-ternario.js
+++ javascript-para-iniciantes/atribuicao-e-ternario/exercicio-atribuicao-e-ternario.js
@@ -1,6 +1,9 @@
 // Some 500 ao valor de scroll abaixo,
 // atribuindo o novo valor a scroll
 var scroll = 1000;
+scroll += 500;
+
+console.log(scroll);    //  1500
 
 // Atribua true para a variável darCredito,
 // caso o cliente possua carro e casa.
@@ -8,3 +11,14 @@ var scroll = 1000;
 var possuiCarro = true;
 var possuiCasa = true;
 var darCredito;
+
+darCredito = (possuiCarro && possuiCasa);
+console.log(darCredito);    //  true
+
+//  agora, se você quiser retornar uma string, ai sim você usa o ternário
+
+darCredito = (possuiCarro && possuiCasa)
+    ? 'Dar crédito ao cliente'
+    : 'Negar crédito';
+
+console.log(darCredito);    //  'Dar crédito ao cliente'
