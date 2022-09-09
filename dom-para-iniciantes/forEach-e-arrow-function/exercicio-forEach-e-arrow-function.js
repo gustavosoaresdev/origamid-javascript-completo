042cd7ba9c980f03e7d6e174fd76a11b1caa84fa
guuhgst <gustavoprogramador18@gmail.com>
2022-09-09 15:37:44 -0300

    docs: :white_check_mark: exercício concluido!

--- dom-para-iniciantes/forEach-e-arrow-function/exercicio-forEach-e-arrow-function.js
+++ dom-para-iniciantes/forEach-e-arrow-function/exercicio-forEach-e-arrow-function.js
@@ -1,17 +1,23 @@
 // Mostre no console cada parágrado do site
+const paragrafos = document.querySelectorAll('p');
+paragrafos.forEach((paragrafo) => console.log(paragrafo));
 
 // Mostre o texto dos parágrafos no console
+paragrafos.forEach((paragrafo) => {
+    console.log(paragrafo.innerText)
+});
 
 // Como corrigir os erros abaixo:
 const imgs = document.querySelectorAll('img');
 
-imgs.forEach(item, index => {
+imgs.forEach((item, index) => {
     console.log(item, index);
 });
 
 let i = 0;
-imgs.forEach( => {
+imgs.forEach(() => {
     console.log(i++);
 });
 
 imgs.forEach(() => i++);
+console.log(i); // 7
