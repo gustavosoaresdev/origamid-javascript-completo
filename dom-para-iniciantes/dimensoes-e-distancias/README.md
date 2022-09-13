1cf9232eb443ee99adaeb518e64d41c195869022
guuhgst <gustavoprogramador18@gmail.com>
2022-09-13 03:22:21 -0300

    docs: offsetTop e offsetLeft

--- dom-para-iniciantes/dimensoes-e-distancias/README.md
+++ dom-para-iniciantes/dimensoes-e-distancias/README.md
@@ -15,3 +15,16 @@ Estas são propriedades e métodos dos objetos **Element** e
 
 *Mesma coisa para o Width,*
 *clientWidth...*
+
+## offsetTop e offsetLeft
+
+```js
+    const section = document.querySelector('.animais');
+
+    // Distância entre o topo do elemento e o topo da página
+    section.offsetTop;
+    
+    // Distância entre o canto esquerdo do elemento
+    // e o canto esquerdo da página
+    section.offsetLeft;
+```
--- dom-para-iniciantes/dimensoes-e-distancias/script.js
+++ dom-para-iniciantes/dimensoes-e-distancias/script.js
@@ -4,3 +4,13 @@ const totalHeight = animaisLista.scrollHeight;
 console.log(totalHeight);
 
 /* ================================================================================= */
+
+const animaisTop = animaisLista.offsetTop;
+console.log(animaisTop);
+
+const firsth2 = document.querySelector('h2');
+const firsth2Left = firsth2.offsetLeft;
+
+console.log(firsth2Left);
+
+/* ================================================================================= */
