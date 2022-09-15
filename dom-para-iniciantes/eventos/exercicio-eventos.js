2c059480d4e8000dd2a5bda10345b4d347fd17a0
guuhgst <gustavoprogramador18@gmail.com>
2022-09-15 05:26:32 -0300

    docs: início de exercício

---
+++ dom-para-iniciantes/eventos/exercicio-eventos.js
@@ -0,0 +1,12 @@
+// Quando o usuário clicar nos links internos do site,
+// adicione a classe ativo ao item clicado e remova dos
+// demais itens caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+
+// Selecione todos os elementos do site começando a partir do body,
+// ao clique mostre exatamente quais elementos estão sendo clicados
+
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+
+// Se o usuário clicar na tecla (t), aumente todo o texto do site.
