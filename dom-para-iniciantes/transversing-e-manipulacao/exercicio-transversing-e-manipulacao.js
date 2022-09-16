5a0b984a903c69b23fdfc6e8cad184c45e39b350
guuhgst <gustavoprogramador18@gmail.com>
2022-09-16 04:48:48 -0300

    docs: :white_check_mark: exercício concluido!

--- dom-para-iniciantes/transversing-e-manipulacao/exercicio-transversing-e-manipulacao.js
+++ dom-para-iniciantes/transversing-e-manipulacao/exercicio-transversing-e-manipulacao.js
@@ -1,7 +1,20 @@
 // Duplique o menu e adicione ele em copy
+const menu = document.querySelector('.menu'),
+    copy = document.querySelector('.copy');
+
+const cloneMenu = menu.cloneNode(true);
+copy.appendChild(cloneMenu);
 
 // Selecione o primeiro DT da dl de Faq
+const faq = document.querySelector('.faq'),
+    firstDT = faq.querySelector('dt');
+
+console.log(firstDT);
 
 // Selecione o DD referente ao primeiro DT
-    
+const dd = firstDT.nextElementSibling;
+console.log(dd);
+
 // Substitua o conteúdo html de .faq pelo de .animais
+const animais = document.querySelector('.animais');
+faq.innerHTML = animais.innerHTML;
