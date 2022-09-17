3946f8f8840a4eea4f5fa2d5a12063846d54ad69
guuhgst <gustavoprogramador18@gmail.com>
2022-09-17 06:33:03 -0300

    feat: :package: adicionando classe ativo a section pelo index

--- dom-para-iniciantes/navegacao-por-tabs/script.js
+++ dom-para-iniciantes/navegacao-por-tabs/script.js
@@ -1,2 +1,10 @@
 const tabMenu = document.querySelectorAll('.js-tabmenu li'),
     tabContent = document.querySelectorAll('.js-tabcontent section');
+
+function activeTab(index) {
+    tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+    });
+
+    tabContent[index].classList.add('ativo');
+}
