baa4a1637aa6e244f4bd67e42282c838f406a6a9
guuhgst <gustavoprogramador18@gmail.com>
2022-09-20 02:19:36 -0300

    docs: antes de iniciarmos...

--- dom-para-iniciantes/accordion-list/script.js
+++ dom-para-iniciantes/accordion-list/script.js
@@ -1,20 +1,24 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li'),
-    tabContent = document.querySelectorAll('.js-tabcontent section');
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li'),
+        tabContent = document.querySelectorAll('.js-tabcontent section');
 
-if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
+    if (tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('ativo');
 
-    function activeTab(index) {
-        tabContent.forEach((section) => {
-            section.classList.remove('ativo');
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+
+            tabContent[index].classList.add('ativo');
+        }
+
+        tabMenu.forEach((li, index) => {
+            li.addEventListener('click', () => {
+                activeTab(index);
+            });
         });
-
-        tabContent[index].classList.add('ativo');
     }
-
-    tabMenu.forEach((li, index) => {
-        li.addEventListener('click', () => {
-            activeTab(index);
-        });
-    });
 }
+
+initTabNav();
