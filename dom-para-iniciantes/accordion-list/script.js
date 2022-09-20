b8aca8cd6f51b58a9f8744a0068a03c6b91d85ca
guuhgst <gustavoprogramador18@gmail.com>
2022-09-20 02:52:04 -0300

    chore: adicionado classe 'ativo' a uma variável

--- dom-para-iniciantes/accordion-list/script.js
+++ dom-para-iniciantes/accordion-list/script.js
@@ -25,14 +25,15 @@ initTabNav();
 
 function initAccordionList() {
     const accordionList = document.querySelectorAll('.js-accordion dt');
+    const activeClass = 'ativo';
 
     if (accordionList.length) {
-        accordionList[0].classList.add('ativo');
-        accordionList[0].nextElementSibling.classList.add('ativo');
+        accordionList[0].classList.add(activeClass);
+        accordionList[0].nextElementSibling.classList.add(activeClass);
 
         function activeAccordion() {
-            this.classList.toggle('ativo');
-            this.nextElementSibling.classList.toggle('ativo');
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);
         }
 
         accordionList.forEach((dt) => {
