a042eec4d8c6156482906c9952d7a2fe462a7366
guuhgst <gustavoprogramador18@gmail.com>
2022-09-24 05:07:23 -0300

    docs: encapsulando todo o código pra dentro de uma função + comentário

--- dom-para-iniciantes/scroll-suave-nos-links-internos/script.js
+++ dom-para-iniciantes/scroll-suave-nos-links-internos/script.js
@@ -44,19 +44,26 @@ function initAccordionList() {
 
 initAccordionList();
 
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-function scrollToSection(event) {
-    event.preventDefault();
+    function scrollToSection(event) {
+        event.preventDefault();
 
-    const href = event.currentTarget.getAttribute('href');
-    const section = document.querySelector(href);
-    section.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+
+        /* esse método não funciona em todos os browsers,
+        mas no Google Chrome e no Firefox funciona! */
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
+    }
+
+    linksInternos.forEach((linkInterno) => {
+        linkInterno.addEventListener('click', scrollToSection);
     })
 }
 
-linksInternos.forEach((linkInterno) => {
-    linkInterno.addEventListener('click', scrollToSection);
-})
+initScrollSuave();
