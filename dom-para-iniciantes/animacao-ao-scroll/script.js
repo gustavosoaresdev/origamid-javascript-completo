20686cae7ca5e8a3a0867c51910495ede0048128
guuhgst <gustavoprogramador18@gmail.com>
2022-09-29 12:26:37 -0300

    docs: :white_check_mark: 'elementos (sections)' verificados!

--- dom-para-iniciantes/animacao-ao-scroll/script.js
+++ dom-para-iniciantes/animacao-ao-scroll/script.js
@@ -71,7 +71,12 @@ initScrollSuave();
 const sections = document.querySelectorAll('.js-scroll');
 
 function animaScroll() {
-
+    sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        if (sectionTop < 0) {
+            section.classList.add('ativo');
+        }
+    })
 }
 
 window.addEventListener('scroll', animaScroll);
