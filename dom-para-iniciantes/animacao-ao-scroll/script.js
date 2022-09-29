a35373a91bfd24853fb1002b3b918b151ab4f47f
guuhgst <gustavoprogramador18@gmail.com>
2022-09-29 13:00:37 -0300

    feat: :package: deixando o código um pouco melhor + comentário

--- dom-para-iniciantes/animacao-ao-scroll/script.js
+++ dom-para-iniciantes/animacao-ao-scroll/script.js
@@ -68,16 +68,38 @@ function initScrollSuave() {
 
 initScrollSuave();
 
-const sections = document.querySelectorAll('.js-scroll');
-const windowMetade = window.innerHeight * 0.6;
+function initAnimacaoScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
 
-function animaScroll() {
-    sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top - windowMetade;
-        if (sectionTop < 0) {
-            section.classList.add('ativo');
+    if (sections.length) {
+        const windowMetade = window.innerHeight * 0.6;
+
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+
+                if (isSectionVisible) {
+                    section.classList.add('ativo');
+                }
+
+                /* se quiser que a animação volta a ocorrer quando o usuário
+                voltar para cima, basta colocar um (else) e remover a classe (ativo). */
+
+                // desse jeito:
+
+                // else {
+                //     section.classList.remove('ativo');
+                // }
+
+                // no meu caso, eu não vou querer.
+            })
         }
-    })
+
+        animaScroll();
+
+        window.addEventListener('scroll', animaScroll);
+    }
 }
 
-window.addEventListener('scroll', animaScroll);
+initAnimacaoScroll();
