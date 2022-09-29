97f1044ecfd91aab2f05edac0a6f1fe6ebfe827e
guuhgst <gustavoprogramador18@gmail.com>
2022-09-29 12:49:26 -0300

    docs: :white_check_mark: 'animação após ultrapassar 0.6 da tela do usuário' concluido!

--- dom-para-iniciantes/animacao-ao-scroll/script.js
+++ dom-para-iniciantes/animacao-ao-scroll/script.js
@@ -69,10 +69,11 @@ function initScrollSuave() {
 initScrollSuave();
 
 const sections = document.querySelectorAll('.js-scroll');
+const windowMetade = window.innerHeight * 0.6;
 
 function animaScroll() {
     sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
         if (sectionTop < 0) {
             section.classList.add('ativo');
         }
