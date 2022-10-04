5f71485c2db6b46cfdfcd22c245afdf33be13f6f
guuhgst <gustavoprogramador18@gmail.com>
2022-10-04 02:28:27 -0300

    docs: constructor functions

--- objetos/constructor-functions/README.md
+++ objetos/constructor-functions/README.md
@@ -24,3 +24,27 @@ possível com o **Object.create**, mas veremos ele mais tarde.
 
 *carro, fiat e honda apontam para* <br>
 *o mesmo objeto.*
+
+## Constructor Functions
+
+Para isso existem as Constructor Functions, ou seja, Funções <br>
+Construtoras que são responsáveis por construir novos objetos <br>
+sempre que chamamos a mesma.
+
+```js
+    function Carro() {
+        this.marca = 'Marca';
+        this.preco = 0;
+    }
+
+    const honda = new Carro();
+    honda.marca = 'Honda';
+    honda.preco = 4000;
+
+    const fiat = new Carro();
+    fiat.marca = 'Fiat';
+    fiat.preco = 3000;
+```
+
+*Usar Pascal Case, ou seja,* <br>
+*começar com letra maiúscula.*
--- objetos/constructor-functions/script.js
+++ objetos/constructor-functions/script.js
@@ -3,15 +3,37 @@ const carro = {
     preco: 0,
 }
 
-const honda = carro;
-honda.marca = 'Honda';
-honda.preco = 2000;
+// const honda = carro;
+// honda.marca = 'Honda';
+// honda.preco = 2000;
 
-const fiat = carro;
-fiat.marca = 'Fiat';
-fiat.preco = 3000;
+// const fiat = carro;
+// fiat.marca = 'Fiat';
+// fiat.preco = 3000;
 
 /* dessa forma acima, não é uma cópia, pois na verdade estamos
 alterando o objeto (carro). */
+
+/* ================================================================================= */
+
+function Carro() {
+    this.marca = 'Marca';
+    this.preco = 2000;
+}
+
+const honda = new Carro();
+honda.marca = 'Honda';
+honda.preco = 3000;
+
+const fiat = new Carro();
+fiat.marca = 'Fiat';
+fiat.preco = 4000;
+
+console.log(honda); // Carro {marca: 'Honda', preco: 3000}
+console.log(fiat); // Carro {marca: 'Fiat', preco: 4000}
+
+/* agora sim! com a constructor functions, podemos criar
+novos objetos baseado nela, e não vai afetar / alterar o
+objeto (Carro) de jeito nenhum. */
 
 /* ================================================================================= */
