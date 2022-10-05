01a8a0a19a955d76198e080dbdeb2823aa31435c
guuhgst <gustavoprogramador18@gmail.com>
2022-10-05 03:14:19 -0300

    docs: constructor function real

--- objetos/constructor-functions/script2.js
+++ objetos/constructor-functions/script2.js
@@ -1,15 +1,26 @@
-const Dom = {
-    seletor: 'li',
-    element() {
+// const Dom = {
+//     seletor: 'li',
+//     element() {
+//         return document.querySelector(this.seletor);
+//     },
+//     ativar() {
+//         this.element().classList.add('ativo');
+//     }
+// }
+
+function Dom() {
+    this.seletor = 'li';
+    this.element = function () {
         return document.querySelector(this.seletor);
-    },
-    ativar() {
+    }
+    this.ativar = function () {
         this.element().classList.add('ativo');
     }
 }
 
-Dom.ativar();
-Dom.seletor = 'ul';
-Dom.ativar();
+const li = new Dom();
+li.ativar();
 
-/* ================================================================================= */
+const ul = new Dom();
+ul.seletor = 'ul';
+ul.ativar();
