95f26ae9834226a3d3194d15c93e3cdbbe7fb106
guuhgst <gustavoprogramador18@gmail.com>
2022-11-14 04:18:28 -0300

    docs & test: Propriedades

--- objetos/function/README.md
+++ objetos/function/README.md
@@ -12,3 +12,17 @@ as suas propriedades e métodos.
 
     const perimetroQuadrado = new Function('lado', 'return lado * 4');
 ```
+
+## Propriedades
+
+**Function.length** retorna o total de argumentos da função. <br>
+**Function.name** retorna uma string com o nome da função.
+
+```js
+    function somar(n1, n2) {
+        return n1 + n2;
+    }
+    
+    somar.length; // 2
+    somar.name; // 'somar'
+```
--- objetos/function/script.js
+++ objetos/function/script.js
@@ -8,4 +8,29 @@ const perimetro = new Function('lado', 'return lado * 4');
 Mas é só pra deixar claro que é a mesma coisa que a função
 lá de cima (areaQuadrado). */
 
+console.log('') // Apenas uma quebra de linha...
+
 /* ================================================================================= */
+
+function somar(n1, n2) {
+    return n1 + n2;
+}
+
+console.log(somar(3, 3));
+
+/* Lembrando, se eu estou executando a função, o que ela me retorna
+é um número, então nesse caso eu vou ter métodos e propriedades de (Number). */
+
+// Diferente disso:
+console.log(somar.length); // 2
+
+// Porque agora, eu estou falando diretamente com a função mesmo...
+
+console.log(somar.name.toUpperCase()); // 'SOMAR'
+
+/* somar.name, me retorna uma string, portanto eu terei acesso aos
+métodos e propriedades de uma (String). */
+
+
+
+// Lembrando mais uma vez, o que importa, é sempre o que retorna!!!
