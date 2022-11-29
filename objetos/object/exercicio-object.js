45f510fc6e838ee1f00a36fad47a36d2b75b2ac3
gustavosoaresdev <gustavoprogramador18@gmail.com>
2022-11-29 10:31:17 -0300

    success: Exercícios concluidos! :heavy_check_mark:

--- objetos/object/exercicio-object.js
+++ objetos/object/exercicio-object.js
@@ -1,9 +1,24 @@
 // Crie uma função que verifique
 // corretamente o tipo de dado
+function checkDataType(data) {
+  return Object.prototype.toString.call(data);
+}
+
+console.log(checkDataType({})); // [object Object]
 
 // Crie um objeto quadrado com
 // a propriedade lados e torne
 // ela imutável
+const square = {}
+Object.defineProperties(square, {
+  sides: {
+    value: 4,
+    enumerable: true,
+  }
+})
+
+square.sides = 2;
+console.log(square); // {sides: 4}
 
 // Previna qualquer mudança
 // no objeto abaixo
@@ -13,6 +28,10 @@ const configuracao = {
   background: '#333'
 }
 
+Object.freeze(configuracao);
+
 // Liste o nome de todas
 // as propriedades do
 // protótipo de String e Array
+console.log(Object.getOwnPropertyNames(String.prototype));
+console.log(Object.getOwnPropertyNames(Array.prototype));
